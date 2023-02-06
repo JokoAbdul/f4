7f38bfa3908ee9f99ec96bd48d4d7b72541a52ec
JokoAbdul <123436498+JokoAbdul@users.noreply.github.com>
2023-02-06 14:56:51 +0700

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,20 +4,7 @@
 from pyrogram.types import InlineKeyboardButton
 
 class Data:
-    HELP = """
-<b> ❏ Perintah untuk Pengguna BOT
- ├ /start - Mulai Bot
- ├ /about - Tentang Bot ini
- └ /help - Bantuan Perintah Bot ini 
- 
- ❏ Perintah Untuk Admin BOT
- ├ /users - Untuk Melihat Statistik Pengguna Bot
- ├ /batch - Untuk Membuat Link Lebih dari Satu File
- ├ /stats - Untuk Melihat Status Bot 
- └ /broadcast - Untuk Mengirim Pesan Broadcast ke Pengguna Bot
-
-👨‍💻 Develoved by <a href='https://t.me/Biawak_Store'>@Biawak_Store</a></b>
-"""
+    HELP = """Jika Ingin Membeli VIP Kami Cek Channel @VIIPBokepMurah"""
 
     close = [
         [InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]
