1bfdb155a81fdf7d16208da30ddbed47a22107e5
JokoAbdul <123436498+JokoAbdul@users.noreply.github.com>
2023-02-06 15:22:49 +0700

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,27 @@
+# (©) Anonymous
+
+from pyrogram.types import InlineKeyboardButton
+
+class Data:
+    HELP = """
+<b> ❏ Perintah untuk Pengguna BOT
+ ├ /start - Mulai Bot
+ ├ /about - Tentang Bot ini
+ └ /help - Bantuan Perintah Bot ini 
+ 
+ ❏ Perintah Untuk Admin BOT
+ ├ /users - Untuk Melihat Statistik Pengguna Bot
+ ├ /batch - Untuk Membuat Link Lebih dari Satu File
+ ├ /stats - Untuk Melihat Status Bot 
+ └ /broadcast - Untuk Mengirim Pesan Broadcast ke Pengguna Bot
+👨‍💻 Develoved by <a href='https://t.me/Biawak_Store'>@Biawak_Store</a></b>
+"""
+
+  
+
+    ABOUT = """
+<b>Tentang Bot ini:
+@{} adalah Bot Telegram Untuk Menyimpan File Yang Dapat Diakses Melalui Link Khusus.
+ • Creator: @{}
+👨‍💻 Develoved by <a href='https://t.me/Biawak_Store'>@Biawak_Store</a></b>
+"""
